1c5ffebfb27d1c5b5fc5d3ae8860c8a52160f94e
Sanaa Hamel <sanaahamel@outlook.com>
2023-05-02 10:53:19 -0400

    removed I2C bus scan debugging aid wasn't reliable to begin with, not all devices respond to a cold read

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -44,33 +44,6 @@ double mcu_temperature() {
     return deg_c;
 }
 
-void DBG_i2c_scan(i2c_inst_t* bus) {
-    printf("\nI2C Bus Scan\n");
-    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
-
-    for (int addr = 0; addr < (1 << 7); ++addr) {
-        if (addr % 16 == 0) {
-            printf("%02x ", addr);
-        }
-
-        // Perform a 1-byte dummy read from the probe address. If a slave
-        // acknowledges this address, the function returns the number of bytes
-        // transferred. If the address byte is ignored, the function returns
-        // -1.
-
-        // Skip over any reserved addresses.
-        if (i2c_address_reserved(addr)) {
-            printf(" ");
-        } else {
-            uint8_t rxdata;
-            auto ret = i2c_read_blocking(bus, addr, &rxdata, 1, false);
-            printf(ret < 0 ? "." : "@");
-        }
-
-        printf(addr % 16 == 15 ? "\n" : "  ");
-    }
-}
-
 VecSensors sensors_init_bus(async_context_t& ctx_async, i2c_inst_t* bus, Sensor::Data state) {
     VecSensors sensors;
     auto probe_for = [&](auto p) {
@@ -96,8 +69,6 @@ bool sensors_init(async_context_t& ctx_async, EnvironmentService::ServiceData& s
 
     printf("Waiting %u ms for sensor init\n", unsigned(SENSOR_POWER_ON_DELAY / 1ms));
     busy_wait(SENSOR_POWER_ON_DELAY);
-
-    DBG_i2c_scan(i2c0);
 
     printf("I2C0 - initializing sensors...\n");
     g_sensors_intake = sensors_init_bus(ctx_async, i2c0,
