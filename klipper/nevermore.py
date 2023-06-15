2ccfce5df3a62c1cb2cb8f90ddf88d5cca3c3f00
Sanaa Hamel <sanaahamel@outlook.com>
2023-06-15 11:58:13 -0400

    add WS2812 support to klipper module

--- klipper/nevermore.py
+++ klipper/nevermore.py
@@ -10,33 +10,31 @@ import datetime
 import logging
 import threading
 import weakref
+from abc import abstractmethod
 from dataclasses import dataclass
 from enum import Enum
 from threading import Thread
-from typing import (
-    Any,
-    Callable,
-    Coroutine,
-    MutableMapping,
-    Optional,
-    TypeVar,
-    overload,
-)
+from typing import Any, Callable, Coroutine, MutableMapping, Optional, TypeVar, overload
 from uuid import UUID
 
+import janus
 from bleak import BleakClient, BleakError
 from bleak.backends.service import BleakGATTCharacteristic, BleakGATTService
 from bleak.exc import BleakDeviceNotFoundError
 from configfile import ConfigWrapper
+from extras.led import LEDHelper
 from gcode import GCodeCommand, GCodeDispatch
 from klippy import Printer
 from reactor import SelectReactor
+from typing_extensions import override
 
 __all__ = [
     "load_config",
 ]
 
 _Float = TypeVar("_Float", bound=float)
+
+RGBW = tuple[float, float, float, float]
 
 LOG = logging.getLogger(__name__)
 
@@ -99,6 +97,7 @@ UUID_SERVICE_WS2812 = UUID("f62918ab-33b7-4f47-9fba-8ce9de9fecbb")
 UUID_CHAR_ENV_DATA_AGGREGATE = UUID("75134bec-dd06-49b1-bac2-c15e05fd7199")
 UUID_CHAR_FAN_POWER = UUID("00002b04-0000-1000-8000-00805f9b34fb")
 UUID_CHAR_FAN_TACHO = UUID("03f61fe0-9fe7-4516-98e6-056de551687f")
+UUID_CHAR_WS2812_COMPONENTS = UUID("00002AEA-0000-1000-8000-00805f9b34fb")
 UUID_CHAR_WS2812_UPDATE = UUID("5d91b6ce-7db1-4e06-b8cb-d75e7dd49aae")
 
 
@@ -230,21 +229,136 @@ def _parse_env_agg(reader: BleAttrReader) -> tuple[SensorState, SensorState]:
     )
 
 
+@dataclass
+class LedUpdateSpan:
+    begin: int
+    end: int  # INVARIANT(begin < end)
+    num_dirty: int = 1
+
+    def worth_extending_to(self, i: int):
+        MAX_LENGTH = 256  # can't send more than 256 octets
+        MIN_LENGTH = 32  # send at least a fair sized chunk due to comm overhead
+        MIN_DENSITY = 0.75
+
+        if i < self.end:
+            return False  # already contains or can't extend `begin`
+
+        length = (i + 1) - self.begin
+        if MAX_LENGTH <= length:
+            return False  # too long
+        if length <= MIN_LENGTH:
+            return True  # even if the density sucks, the comm overhead sucks more
+
+        density = (self.num_dirty + 1) / length
+        return MIN_DENSITY <= density
+
+    def extend_to(self, i: int):
+        if not self.worth_extending_to(i):
+            return False
+
+        self.end = i + 1
+        self.num_dirty += 1
+        return True
+
+    @staticmethod
+    def compute_diffs(old_data: bytearray, new_data: bytearray):
+        assert len(old_data) == len(new_data)
+        span: Optional[LedUpdateSpan] = None
+
+        def cmd(span: LedUpdateSpan):
+            return (span.begin, new_data[span.begin : span.end])
+
+        for i, (old, new) in enumerate(zip(old_data, new_data)):
+            if old == new or (span is not None and span.extend_to(i)):
+                continue
+
+            if span is not None:
+                yield cmd(span)
+
+            span = LedUpdateSpan(i, i + 1)
+
+        if span is not None:
+            yield cmd(span)
+
+
+# Commands which aren't directly forwarded to the controller.
+class PseudoCommand:
+    pass
+
+
+# Commands which are directly forwarded to the controller.
+class Command:
+    @abstractmethod
+    def params(self) -> bytearray:
+        raise NotImplemented
+
+
+@dataclass
+class CmdFanPower(Command):
+    percent: float
+
+    def params(self):
+        p = _clamp(self.percent, 0, 1) * 100
+        return int(p * 2).to_bytes(1, "little")
+
+
+@dataclass
+class CmdWs2812Length(Command):
+    n_total_components: int
+
+    def params(self):
+        return int(self.n_total_components).to_bytes(2, "little")
+
+
+# Special pseudo command: Due to the very high frequency of these commands, we don't
+# queue them, but rather mark the pixel buffer as dirty to fold consecutive writes.
+# We still technically need this command because a single pixel buffer update
+# may require several updates to transfer, depending on dirty size.
+# NB: With no-response-write we're technically fast enough for 24 FPS updates,
+#     but let's keep this mechanism anyways.
+class CmdWs2812MarkDirty(PseudoCommand):
+    pass
+
+
+# An `asyncio.Event` that avoid spamming the `ready` queue of a loop.
+# Naively using `call_soon_threadsafe` creates an entry per call even if we're already set.
+# This is almost certainly subtly broken, but works for our limited use case.
+class UNSAFE_LazyAsyncioEvent(asyncio.Event):
+    def __init__(self):
+        super().__init__()
+        self._set_in_ready_queue = False
+
+    @override
+    def clear(self):
+        super().clear()
+        self._set_in_ready_queue = False
+
+    def set_threadsafe(self, loop: asyncio.AbstractEventLoop):
+        # abuse GIL
+        if self._set_in_ready_queue:
+            return
+        self._set_in_ready_queue = True
+
+        loop.call_soon_threadsafe(lambda: self.set())
+
+
+# HACK: This class *heavily* abuses the GIL for data synchronisation between
+# the klipper thread and the background worker thread.
 class NevermoreBackgroundWorker:
     def __init__(self, nevermore: "Nevermore") -> None:
         # A weak ref allows us to end the worker if the nevermore instance is
         # ever GC'd without asking us to disconnect (for whatever reason).
-        self._nevermore = weakref.ref(nevermore)
+        self._nevermore = weakref.ref(nevermore, lambda nevermore: self.disconnect())
         self._address = nevermore.bt_address
         self._connected = threading.Event()
-        self._loop_exit = asyncio.Event()  # not thread safe, unlike `threading.Event`
+        self._loop_exit = UNSAFE_LazyAsyncioEvent()
+        self._led_dirty = UNSAFE_LazyAsyncioEvent()
+        self._led_colour_data_old = bytearray()
         self._loop = asyncio.new_event_loop()
         self._thread = Thread(target=self._worker)
-        # `_set_fan_power` can refer to a dead conn if the worker died for whatever reason.
-        # This isn't great, but it won't be considered fatal and must not interrupt a print.
-        self._set_fan_power: Optional[
-            Callable[[float], Coroutine[Any, Any, None]]
-        ] = None
+        # HACK: Can't set this up yet b/c the loop isn't set.
+        # It'll be available at some point before `self._connected` is set.
+        self._command_queue: janus.Queue[Command] = None
 
         self._thread.start()
 
@@ -253,11 +367,18 @@ class NevermoreBackgroundWorker:
         return self._thread.is_alive() and self._connected.wait(timeout)
 
     def disconnect(self):
-        self._loop.call_soon_threadsafe(lambda: self._loop_exit.set())
+        self._loop_exit.set_threadsafe(self._loop)
 
-    def set_fan_power(self, percent: float):
-        if self._set_fan_power is not None:
-            asyncio.run_coroutine_threadsafe(self._set_fan_power(percent), self._loop)
+    # PRECONDITION: `self._connected` is set
+    def send_command(self, cmd: Command | PseudoCommand):
+        assert self._command_queue is not None, "cannot send commands before connecting"
+        match cmd:
+            case Command() as cmd:
+                self._command_queue.sync_q.put(cmd)
+            case CmdWs2812MarkDirty():
+                self._led_dirty.set_threadsafe(self._loop)
+            case _:
+                raise Exception(f"unhandled pseudo-command {cmd}")
 
     def _worker(self) -> None:
         nevermore = self._nevermore()
@@ -271,9 +392,12 @@ class NevermoreBackgroundWorker:
             LOG.getChild(f"worker"), f"{self._thread.name} - "
         )
 
-        async def go() -> None:
+        async def handle_connection() -> None:
+            # set this up ASAP once we're in an asyncio loop
+            self._command_queue = janus.Queue()
+
             # Attempt (re)connection. Might have to do this multiple times if we lose connection.
-            while not self._worker_should_exit():
+            while True:
                 try:
                     async with BleakClient(
                         self._address, timeout=TIMEOUT_CONNECT
@@ -283,14 +407,21 @@ class NevermoreBackgroundWorker:
                     pass  # ignore, keep trying to (re)connect
                 except BleakDeviceNotFoundError:
                     pass  # ignore, keep trying to (re)connect
-                finally:
-                    # Not rigorous, but at least try to clean up a dead connection.
-                    self._set_fan_power = None
+
+        async def go():
+            main = asyncio.create_task(handle_connection())
+
+            async def canceller():
+                await self._loop_exit.wait()
+                main.cancel()
+
+            await asyncio.gather(main, canceller())
 
         try:
             asyncio.set_event_loop(self._loop)
             self._loop.run_until_complete(go())
         except asyncio.CancelledError:
+            worker_log.exception("worker canceled")
             raise
         except:  # TODO: ignore cancellation & interrupt exception
             worker_log.exception("worker failed")
@@ -321,26 +452,16 @@ class NevermoreBackgroundWorker:
         env_aggregate = require_char(service_env, UUID_CHAR_ENV_DATA_AGGREGATE)
         fan_power = require_char(service_fan, UUID_CHAR_FAN_POWER)
         fan_tacho = require_char(service_fan, UUID_CHAR_FAN_TACHO)
+        ws2812_length = require_char(service_ws2812, UUID_CHAR_WS2812_COMPONENTS)
         ws2812_update = require_char(service_ws2812, UUID_CHAR_WS2812_UPDATE)
 
-        async def set_fan_power(percent: float):
-            percent = _clamp(percent, 0, 1) * 100
-            try:
-                await client.write_gatt_char(
-                    fan_power, int(percent * 2).to_bytes(1, "little")
-                )
-            except BleakError:
-                # consider non-fatal. don't to abort due to potentially transient error
-                log.exception(f"failed to set fan-power={percent}")
-
-        self._set_fan_power = set_fan_power
         self._connected.set()
 
         async def read(char: BleakGATTCharacteristic):
             return BleAttrReader(await client.read_gatt_char(char))
 
         # FUTURE WORK: replace polling w/ GATT notify/indicate mechanism.
-        while not self._worker_should_exit():
+        async def poll_state():
             try:
                 (intake, exhaust) = _parse_env_agg(await read(env_aggregate))
                 # normalise to [0,1] range
@@ -349,8 +470,7 @@ class NevermoreBackgroundWorker:
             except BleakError as e:
                 # special case: lost connection -> wait and attempt reconnect
                 if e.args[0] == "Not connected":
-                    log.info("connection lost. attempting reconnection...")
-                    return
+                    raise
 
                 log.exception(f"failed to read controller status")
                 (intake, exhaust) = (SensorState(), SensorState())
@@ -359,7 +479,7 @@ class NevermoreBackgroundWorker:
 
             nevermore = self._nevermore()
             if nevermore is None:
-                break  # nevermore instance got GC'd at some point. we're done.
+                return  # frontend is dead -> nothing to do
 
             # HACK: Abuse GIL to keep this thread-safe
             nevermore.state = ControllerState(
@@ -368,12 +488,73 @@ class NevermoreBackgroundWorker:
                 fan_power=fan_power_,
                 fan_tacho=fan_tacho_,
             )
-            nevermore = None  # release reference b/c python doesn't have nice scoping
 
             await asyncio.sleep(1 / NEVERMORE_POLL_HZ)
 
-    def _worker_should_exit(self):
-        return self._loop_exit.is_set() or self._nevermore() is None
+        async def handle_commands():
+            cmd = await self._command_queue.async_q.get()
+            try:
+                match cmd:
+                    case CmdFanPower():
+                        await client.write_gatt_char(fan_power, cmd.params())
+                    case CmdWs2812Length():
+                        await client.write_gatt_char(ws2812_length, cmd.params())
+                    case _:
+                        raise Exception(f"unhandled command {cmd}")
+            except BleakError as e:
+                # consider non-fatal. don't to abort due to potentially transient error
+                # special case: lost connection -> wait and attempt reconnect
+                if e.args[0] == "Not connected":
+                    raise
+
+                log.exception(f"command failed cmd={cmd}")
+
+        async def handle_led():
+            await self._led_dirty.wait()
+            self._led_dirty.clear()
+
+            for offset, data in self._worker_led_diffs():
+                params = bytearray([offset, len(data)]) + data
+                await client.write_gatt_char(ws2812_update, params)
+
+        async def forever(go: Callable[[], Coroutine[Any, Any, Any]]):
+            while True:
+                await go()
+
+        tasks = asyncio.gather(
+            *[forever(x) for x in [poll_state, handle_commands, handle_led]]
+        )
+
+        try:
+            await tasks
+        except BleakError as e:
+            # consider non-fatal. don't to abort due to potentially transient error
+            # special case: lost connection -> wait and attempt reconnect
+            if e.args[0] == "Not connected":
+                log.info("connection lost. attempting reconnection...")
+                return
+
+            raise  # any other kind of uncaught failure -> re-raise
+        finally:
+            tasks.cancel()  # kill off all active tasks if any fail
+
+    def _worker_led_diffs(self):
+        nm = self._nevermore()
+        if nm is None:
+            return  # frontend is dead -> nothing to do
+
+        # resize to accommodate. change of length -> just mark everything as dirty
+        if len(self._led_colour_data_old) != len(nm.led_colour_data):
+            self._led_colour_data_old = bytearray([x ^ 1 for x in nm.led_colour_data])
+
+        if self._led_colour_data_old == nm.led_colour_data:
+            return  # fast-path bail
+
+        yield from LedUpdateSpan.compute_diffs(
+            self._led_colour_data_old, nm.led_colour_data
+        )
+
+        self._led_colour_data_old[:] = nm.led_colour_data
 
 
 class Nevermore:
@@ -387,11 +568,34 @@ class Nevermore:
 
         if not _bt_address_validate(self.bt_address):
             raise config.error(
-                f"`bt_address` isn't a valid bluetooth address, given `{self.bt_address}`"
+                f"invalid bluetooth address for `bt_address`, given `{self.bt_address}`"
             )
 
+        # LED-specific code.
+        # Ripped from `extras/neopixel.py` because the processing code is entangled with MCU transmission.
+        # Modified to remove minor absurdities & fit our use case.
+        led_chain_count = config.getint("led_chain_count", 0, minval=0)
+        led_colour_order = config.getlist("led_colour_order", ["GRB"])
+        for x in led_colour_order:
+            if sorted(x) not in (sorted("RGB"), sorted("RGBW")):
+                raise config.error(f"invalid `led_colour_order` '{x}'")
+        if len(led_colour_order) == 1:
+            led_colour_order = [led_colour_order[0]] * led_chain_count
+        if len(led_colour_order) != led_chain_count:
+            raise config.error("`led_colour_order` does not match `led_chain_count`")
+
+        self.led_colour_idxs = [
+            (i, "RGBW".index(c))
+            for i, colour_order in enumerate(led_colour_order)
+            for c in colour_order
+        ]
+        self.led_colour_data = bytearray(len(self.led_colour_idxs))
+        self.led_helper: LEDHelper = self.printer.load_object(
+            config, "led"
+        ).setup_helper(config, self._led_update, led_chain_count)
+
         self._interface: Optional[NevermoreBackgroundWorker] = None
-        self._handle_request_restart(0)
+        self._handle_request_restart(None)
 
         self.printer.add_object(f"fan_generic {self.fan.name}", self.fan)
         self.printer.register_event_handler("klippy:connect", self._handle_connect)
@@ -402,18 +606,30 @@ class Nevermore:
 
     def set_fan_power(self, percent: float):
         if self._interface is not None:
-            self._interface.set_fan_power(percent)
+            self._interface.send_command(CmdFanPower(percent))
+
+    def _led_update(self, led_state: list[RGBW], print_time: Optional[float]) -> None:
+        for i, (led, clr) in enumerate(self.led_colour_idxs):
+            self.led_colour_data[i] = int(led_state[led][clr] * 255.0 + 0.5)
+
+        if self._interface is None:
+            LOG.info(f"_led_update failed: not connected")
+            return
+
+        # Background interface will handle diffing and update as appropriate
+        self._interface.send_command(CmdWs2812MarkDirty())
 
     def _handle_connect(self) -> None:
-        if self._interface is not None and not self._interface.wait_for_connection(
-            TIMEOUT_CONNECT
-        ):
+        if self._interface is None:
+            raise Exception("precondition - `self._interface` must not be None")
+
+        if not self._interface.wait_for_connection(TIMEOUT_CONNECT):
             raise self.printer.config_error("nevermore failed to connect - timed out")
 
-        # self.reactor.update_timer(self.sample_timer, self.reactor.NOW)
-        pass
+        self._interface.send_command(CmdWs2812Length(len(self.led_colour_idxs)))
+        self._led_update(self.led_helper.get_status()["color_data"], None)
 
-    def _handle_request_restart(self, print_time):
+    def _handle_request_restart(self, print_time: Optional[float]):
         self._handle_shutdown()
         self._interface = NevermoreBackgroundWorker(self)
         # TODO: reset fan & LED to defaults?
