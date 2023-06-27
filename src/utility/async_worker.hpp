c5bf51f331ca8a0e3a3d976873365952492ad4a8
Sanaa Hamel <sanaahamel@outlook.com>
2023-06-27 11:52:38 -0400

    feat: async-worker can now change delay

--- src/utility/async_worker.hpp
+++ src/utility/async_worker.hpp
@@ -5,16 +5,21 @@
 #include <cassert>
 #include <chrono>
 #include <cstdint>
+#include <ratio>
 
-template <uint32_t DELAY_MS, typename A>
+template <typename A>
 struct AsyncWorker {
+    using Delay = std::chrono::duration<uint32_t, std::milli>;
     using FnPtr = A (*)();
 
-    AsyncWorker(FnPtr go) {
-        worker.user_data = reinterpret_cast<void*>(go);
+    Delay delay;
+
+    AsyncWorker(FnPtr go, Delay delay) : delay(delay), go(go) {
+        worker.user_data = reinterpret_cast<void*>(this);
         worker.do_work = [](async_context_t* context, async_at_time_worker_t* worker) {
-            reinterpret_cast<FnPtr>(worker->user_data)();
-            register_(*context, *worker);
+            auto& self = *reinterpret_cast<AsyncWorker*>(worker->user_data);
+            self.go();
+            register_(*context, *worker, self.delay);
         };
     }
 
@@ -28,25 +33,30 @@ struct AsyncWorker {
         worker.do_work(&context, &worker);
     }
 
-    void register_(async_context_t& context,
-            std::chrono::milliseconds initial_delay = std::chrono::milliseconds(DELAY_MS)) {
-        register_(context, worker, initial_delay / std::chrono::milliseconds(1));
+    void register_(async_context_t& context) {
+        register_(context, delay);
+    }
+
+    void register_(async_context_t& context, Delay initial_delay) {
+        register_(context, worker, initial_delay);
     }
 
 private:
+    FnPtr go;
     async_at_time_worker_t worker{};
 
-    static void register_(async_context_t& context, async_at_time_worker_t& worker, uint32_t ms = DELAY_MS) {
-        async_using(context, [&]() { async_context_add_at_time_worker_in_ms(&context, &worker, ms); });
+    static void register_(async_context_t& context, async_at_time_worker_t& worker, Delay ms) {
+        async_using(
+                context, [&]() { async_context_add_at_time_worker_in_ms(&context, &worker, ms.count()); });
     }
 };
 
 template <uint32_t DELAY_MS, typename A>
 auto mk_sync_worker(A (*go)()) {
-    return AsyncWorker<DELAY_MS, decltype(A())>(go);
+    return AsyncWorker<decltype(A())>(go, std::chrono::milliseconds(DELAY_MS));
 }
 
 template <uint32_t DELAY_MS>
 auto mk_async_worker(void (*go)()) {
-    return AsyncWorker<DELAY_MS, void>(go);
+    return AsyncWorker<void>(go, std::chrono::milliseconds(DELAY_MS));
 }
