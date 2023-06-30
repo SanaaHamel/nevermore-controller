f4be34128b83d2b9e413b93b73a1f23e76578f57
Sanaa Hamel <sanaahamel@outlook.com>
2023-06-30 10:59:33 -0400

    fix: disallow copy/move of periodic sensors
    Safety issue. Once registered they're pinned in memory.

--- src/sensors/async_sensor.hpp
+++ src/sensors/async_sensor.hpp
@@ -11,7 +11,12 @@ struct Sensor {
 
 // A sensor that schedules itself for periodic updates via an async context.
 // Useful for sensors that take a long time (10ms+) to measure/respond.
+// NOLINTNEXTLINE(cppcoreguidelines-special-member-functions)
 struct SensorPeriodic : Sensor {
+    SensorPeriodic() = default;
+    SensorPeriodic(SensorPeriodic const&) = delete;  // copying is almost certainly a mistake
+    SensorPeriodic(SensorPeriodic&&) = delete;       // not safe to move b/c we're pinned once registered
+
     [[nodiscard]] virtual std::chrono::milliseconds update_period() const {
         return SENSOR_UPDATE_PERIOD;
     }
