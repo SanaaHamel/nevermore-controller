13b8c712a4ee3e2d5ca1ea79ff0d320fabd2b954
Sanaa Hamel <sanaahamel@outlook.com>
2023-08-01 15:40:11 -0400

    chore(ota): more setup feedback

--- tools/setup-tool-env.bash
+++ tools/setup-tool-env.bash
@@ -16,14 +16,19 @@ fi
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 VENV_DIR="$ROOT_DIR/.venv"
 
+if [ ! -d "$VENV_DIR" ]; then
+  echo "Setting up \`${VENV_DIR}\`..."
+  echo "This may take a bit..."
+fi
+
 if [ ! -d "$VENV_DIR" ] && ! virtualenv -p python3 "$VENV_DIR"; then
   echo "[ERR] Failed to create \`${VENV_DIR}\`"
   rm -rf "$VENV_DIR"
   exit 1
 fi
 
-if ! "$VENV_DIR/bin/pip" install -q -r "$ROOT_DIR/requirements.txt" ||
-  ! "$VENV_DIR/bin/pip" install -q -r "$ROOT_DIR/../klipper/requirements.txt" ||
+if ! "$VENV_DIR/bin/pip" install -r "$ROOT_DIR/requirements.txt" ||
+  ! "$VENV_DIR/bin/pip" install -r "$ROOT_DIR/../klipper/requirements.txt" ||
   ! "$VENV_DIR/bin/pip" install git+https://github.com/sanaahamel/serial-flash-py; then
   echo "[ERR] Failed to install dependencies."
   echo "If there's a conflict, try deleting \`${VENV_DIR}\` entirely and re-runnning the script."
