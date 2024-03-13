3896d72821de18d7317c785a0a890a4c944eb1b9
Sanaa Hamel <sanaahamel@outlook.com>
2024-03-13 09:30:10 -0400

    chore: moved LVGL mem monitor to right-mid

--- src/config/lib/lv_conf.h
+++ src/config/lib/lv_conf.h
@@ -284,7 +284,7 @@ extern "C" {
  * Requires LV_MEM_CUSTOM = 0*/
 #define LV_USE_MEM_MONITOR 0
 #if LV_USE_MEM_MONITOR
-    #define LV_USE_MEM_MONITOR_POS LV_ALIGN_BOTTOM_LEFT
+    #define LV_USE_MEM_MONITOR_POS LV_ALIGN_RIGHT_MID
 #endif
 
 /*1: Draw random colored rectangles over the redrawn areas*/
