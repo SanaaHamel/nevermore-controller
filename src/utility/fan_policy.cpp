9164e57414065cbf76c475b0ed99774b0cdd89c0
Sanaa Hamel <sanaahamel@outlook.com>
2025-02-15 15:03:53 -0500

    feat: proportional automatic fan control

--- src/utility/fan_policy.cpp
+++ src/utility/fan_policy.cpp
@@ -13,6 +13,19 @@ namespace nevermore {
 
 namespace {
 
+// configurable
+constexpr float FILTER_POWER_MINIMUM = 0.1f;
+constexpr float PROPORTIONAL_CONTROL_STDDEV = 1.5;  // max @ this many stddev away from passive-max threshold
+
+// non-configurable
+constexpr float VOC_MAX = 500;
+constexpr float VOC_NOMINAL_MAX = 200;
+constexpr float VOC_1_5_ABOVE_NOMINAL_MAX = 300;
+// HACK: [100, 440] VOC index range is almost linear, no need to sigmoid^-1.
+//       We'll cheat and assume the rest is linear too.
+constexpr float PROPORTIONAL_CONTROL_IN_VOC =
+        PROPORTIONAL_CONTROL_STDDEV * (VOC_1_5_ABOVE_NOMINAL_MAX - VOC_NOMINAL_MAX);
+
 constexpr bool policy_voc_too_high(VOCIndex voc_passive_max, VOCIndex intake, VOCIndex exhaust) {
     return voc_passive_max <= max(intake.value_or(0), exhaust.value_or(0));
 }
@@ -49,10 +62,17 @@ float FanPolicyEnvironmental::Instance::operator()(
         nevermore::sensors::Sensors const& state, Clock::time_point now) {
     switch (evaluate(*this, state, now)) {
     case Idle: return 0;
-    case Cooldown: return 1;
+    case Cooldown: return FILTER_POWER_MINIMUM;
     case Filtering: {
         last_filter = now;
-        return 1;
+
+        auto const voc_limit = max(VOC_NOMINAL_MAX, float(params.voc_passive_max.value_or(0)));
+        if (VOC_MAX <= voc_limit) return 1;  // weird. whatever, don't second guess it.
+
+        auto const voc_control_range = min(PROPORTIONAL_CONTROL_IN_VOC, VOC_MAX - voc_limit);
+        auto const voc_curr =
+                float(max(state.voc_index_intake.value_or(0), state.voc_index_exhaust.value_or(0)));
+        return clamp((voc_curr - voc_limit) / voc_control_range, FILTER_POWER_MINIMUM, 1.f);
     }
     }
 
